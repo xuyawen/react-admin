9122e1b9665771bf00420656cbd4fd0c05c01047
xuyawen <xuyawenwy@163.com>
2019-08-05 19:08:05 +0800

    添加公共样式

--- src/admin.js
+++ src/admin.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import './style/common.less';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NavLeft from './components/NavLeft';
 
 class Admin extends React.Component {
   render() {
     return (
-      <Row>
-        <Col span={3}>
-          Left
+      <Row className="container">
+        <Col span={3} className="nav-left">
+        <NavLeft />
         </Col>
-        <Col span={21}>
-          <Header></Header>
-          <Row>
-            Main
+        <Col span={21} className="main">
+          <Header />
+          <Row className="content">
+            {/* {this.props.children} */}
+            Content
           </Row>
-          <Footer></Footer>
+          <Footer />
         </Col>
       </Row>
     );
