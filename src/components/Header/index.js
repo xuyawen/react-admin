f2e206d251cece7f65a160d3cdd2d5b88fe7772e
xuyawen <xuyawenwy@163.com>
2019-08-06 19:10:29 +0800

    侧边栏渲染

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { Row, Col, Button } from 'antd';
+import Util from '../../utils/utils'
+import './index.less';
 
 class Header extends Component {
+  state = {
+    userName: 'xuyawen'
+  }
+  componentWillMount() {
+    setInterval(() => {
+      let sysTime = Util.formateDate(new Date().getTime());
+      this.setState({ sysTime })
+    }, 1000)
+  }
   render() {
     return (
-      <div>Header</div>
+      <div className="header">
+        <Row className="header-top">
+          <Col span={24}>
+            <span>欢迎，{ this.state.userName }</span>
+            <Button type="link">退出</Button>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span={4} className="breadcrumb-title">
+            首页
+          </Col>
+          <Col span={20} className="weather">
+            <span className="date">{ this.state.sysTime }</span>
+            <span className="weather-detail">晴转多云</span>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
