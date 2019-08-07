2bbdcb136abd0c1ff7cf1677ca9d0c82ca6f52b7
xuyawen <xuyawenwy@163.com>
2019-08-08 00:07:07 +0800

    天气API参数

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,7 @@ export default {
   formateDate(time) {
     if (!time) return '';
     let date = new Date(time);
-    return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    const format = date => +date < 10 ? '0' + date : date;
+    return date.getFullYear() + '-' + format(date.getMonth()+1) + '-' + format(date.getDate()) + ' ' + format(date.getHours()) + ':' + format(date.getMinutes()) + ':' + format(date.getSeconds());
   }
 }
